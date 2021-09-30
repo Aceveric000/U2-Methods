a9a8ce728fe0795b82ef6eb42dbc97158677cba1
aceveric000 <Aceveric000@warren.k12.in.us>
2021-09-30 11:43:12 -0400

    calculator

--- L6-Swing Text/src/BasicCalculator.java
+++ L6-Swing Text/src/BasicCalculator.java
@@ -1,6 +1,10 @@
-public class BasicCalculator {
+import sun.plugin2.message.JavaObjectOpMessage;
 
-    /* Create a calculator program with two text fields
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/* Create a calculator program with two text fields
     and 4 buttons, one for each of the arithmetic operations.
 
     The user will enter numbers, could be decimals, then click
@@ -8,4 +12,100 @@ public class BasicCalculator {
 
     NO POP-UPS
      */
+    public class BasicCalculator {
+
+    static JTextField inputField1, inputField2;
+    static JLabel result;
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Calc");
+        JPanel panel = new JPanel();
+        JLabel inputNum1 = new JLabel ("enter num");
+        JLabel inputnum2 = new JLabel("enter num");
+        inputField1 = new JTextField(20);
+        inputField2 = new JTextField(20);
+
+        JButton button = new JButton("add");
+        JButton button2 = new JButton("Divide");
+        JButton button3 = new JButton("multiply");
+        JButton button4 = new JButton("Subtract");
+
+        result = new JLabel("0.00");
+
+
+        window.setSize(300, 400);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+
+        button.addActionListener(new BasicCalculator.buttonListener());
+        button2.addActionListener(new button2Listener());
+        button3.addActionListener(new button3Listener());
+       button4.addActionListener(new button4Listener());
+        panel.add(inputNum1);
+        panel.add(inputField1);
+        panel.add(inputnum2);
+        panel.add(inputField2);
+        panel.add(result);
+
+        panel.add(button);
+        panel.add(button2);
+        panel.add(button3);
+        panel.add(button4);
+
+
+
+
+        window.add(panel);
+
+        window.setVisible(true);
+    }
+
+    private static class buttonListener implements ActionListener {
+        /*
+          1.get input
+           2. do math'
+           3.put result
+         */
+        public void actionPerformed(ActionEvent e) {
+            double num1 = Double.parseDouble(inputField1.getText());
+            double num2 = Double.parseDouble(inputField2.getText());
+
+        result.setText(String.valueOf(num1 + num2));
+
+        }
+    }
+    static class button2Listener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            double num1 = Double.parseDouble(inputField1.getText());
+            double num2 = Double.parseDouble(inputField2.getText());
+
+            result.setText(String.valueOf(num1 / num2));
+
+
+        }
+    }
+    static class button3Listener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            double num1 = Double.parseDouble(inputField1.getText());
+            double num2 = Double.parseDouble(inputField2.getText());
+
+            result.setText(String.valueOf(num1 * num2));
+
+
+        }
+    }
+    static class button4Listener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            double num1 = Double.parseDouble(inputField1.getText());
+            double num2 = Double.parseDouble(inputField2.getText());
+
+            result.setText(String.valueOf(num1 - num2));
+
+
+        }
+    }
 }
+
--- L6-Swing Text/src/SimpleInput.java
+++ L6-Swing Text/src/SimpleInput.java
@@ -1,4 +1,51 @@
+import javafx.scene.control.Labeled;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class SimpleInput {
 
+    static int clickCount = 0;
+            static JLabel clickCountLabel;
+            static JTextField input;
 
-}
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("clicks");
+        JPanel panel = new JPanel();
+        JLabel inputLabel = new JLabel("enter name");
+    clickCountLabel = new JLabel("click count");
+       input = new JTextField(15);
+        JButton button = new JButton("Click for boom ");
+
+        window.setSize(300,400);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+
+        button.addActionListener(new ButtonListener());
+
+        panel.add(inputLabel);
+        panel.add(input);
+        panel.add(clickCountLabel);
+        panel.add(button);
+
+        window.add(panel);
+
+        window.setVisible(true);
+
+    }
+
+    static class ButtonListener implements ActionListener{
+
+        public void actionPerformed(ActionEvent e){
+            String product = input.getText();
+
+            clickCount++;
+            clickCountLabel.setText(" i changed it " + clickCount + " Number of times " + product);
+
+            }
+
+        }
+
+    }
