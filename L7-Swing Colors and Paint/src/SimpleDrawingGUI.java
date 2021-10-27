9712cec61e2c8c18c5e1334ec64102623193aaaa
aceveric000 <Aceveric000@warren.k12.in.us>
2021-10-27 11:42:30 -0400

    Artwork

--- L7-Swing Colors and Paint/src/SimpleDrawingGUI.java
+++ L7-Swing Colors and Paint/src/SimpleDrawingGUI.java
@@ -12,19 +12,56 @@ public class SimpleDrawingGUI {
         frame.setLocationRelativeTo(null);
 
         //ADD THE PANEL TO THE FRAME
+        JPanel panel = new DrawingPanel();
 
-
+        frame.add(panel);
         frame.setVisible(true);
     }
 
-    private class DrawingPanel extends JPanel{
+    private class DrawingPanel extends JPanel {
 
         //Constructor for the class
-        public DrawingPanel(){
+        public DrawingPanel() {
             //set the Background color
-
+            Color rainy = new Color(36,64,92);
+            setBackground(rainy);
         }
 
+        public void paintComponent(Graphics g){
 
+            super.paintComponent(g);
+
+
+            Color cool_blue = new Color(25,62,4);
+            g.fillRect(400,400,400,400);
+            g.setColor(cool_blue);
+            g.fillRect(1,450,400,300);
+
+
+            Color wood = new Color(202 , 164, 114);
+            g.setColor(wood);
+            g.fillRect(150,300,100,150);
+
+
+            Color sun = new Color(244 , 246, 240);
+            g.setColor(sun);
+            g.fillRect(10,10,50,50);
+
+            Color Start = new Color(147,185,223);
+            g.setColor(Start);
+            g.drawLine(250,300,60,200);
+
+
+            Color end = new Color(13,28,44);
+            g.setColor(end);
+            g.drawLine(250,1,60,200);
+
+
+
+            g.fillRect(130,150,150,150);
+
+
+
+        }
     }
 }
